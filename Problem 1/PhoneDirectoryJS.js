284ce495660a2ddb2cf347417a184fd3cce12c15
AmarPlakalo <114601596+AmarPlakalo@users.noreply.github.com>
2022-11-01 12:25:19 +0000

    Adding mobile search functionality in JS + validating phone and name

--- Problem 1/PhoneDirectoryJS.js
+++ Problem 1/PhoneDirectoryJS.js
@@ -1,5 +1,21 @@
 function add_contact()
-{	
+{
+	var x = document.forms["contact_form"]["mobile"].value;
+	var y = document.forms["contact_form"]["name"].value;
+	var z = document.forms["contact_form"]["email"].value;
+	
+	if (x.length != 10) 
+	{
+		alert("Mobile number needs to be 10 digits long");
+		return false;
+	}
+	
+	if (/^[A-Za-z\s]*$/.test(y) == false)
+	{
+		alert("Name isn't valid. The name can only contain alphabets and spaces");
+		return false;
+	}
+	
 	var name = document.getElementById('name').value;
 	var mobile = document.getElementById('mobile').value;
 	var email = document.getElementById('email').value;
@@ -28,4 +44,36 @@ function add_contact()
 	document.getElementById('name').value = '';
 	document.getElementById('mobile').value = '';
 	document.getElementById('email').value = '';
+}
+
+function mobile_phone_search() 
+{
+	var input = document.getElementById("search");
+	
+	var table = document.getElementById("contact_list");
+	
+	var tr = table.getElementsByTagName("tr");
+	
+	var td;
+	
+	var cell;
+	
+	var i;
+	
+	for (i = 1; i < tr.length; i++) 
+	{
+		tr[i].style.display = "none";
+  
+		td = tr[i].getElementsByTagName("td");
+
+		cell = tr[i].getElementsByTagName("td")[1];
+		
+		if (cell) 
+		{
+			if (cell.innerHTML.indexOf(input.value) > -1) 
+			{
+				tr[i].style.display = "";
+			} 
+		}
+	}
 }
